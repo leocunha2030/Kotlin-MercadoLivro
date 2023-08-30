d33be7b9fdb542b8c2b153dd38f8bfb5a70ae16a
leocunha2030 <leocunha20030@hotmail.com>
2023-08-30 20:32:08 -0300

    Upgrading lists

--- src/main/kotlin/com/mercadolivro/controller/CustomerController.kt
+++ src/main/kotlin/com/mercadolivro/controller/CustomerController.kt
@@ -2,26 +2,33 @@ package com.mercadolivro.controller
 
 import com.mercadolivro.controller.request.PostCustomerController
 import model.CustomerModel
+import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("customer")
 class CustomerController {
 
+    val customers = mutableListOf<CustomerModel>()
+
     @GetMapping
-    fun getCustomer(): CustomerModel{
-        return  CustomerModel(
-            "1",
-            "Leonardo",
-            "email@email.com")
+    fun getCustomer(): MutableList<CustomerModel> {
+        return  customers
     }
     @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
     fun create(@RequestBody customer: PostCustomerController){
-        println(customer)
+        var id = if (customers.isEmpty()){
+            "1"
+        }else{
+            customers.last().id.toInt() + 1
+        }.toString()
+        customers.add(CustomerModel(id,customer.name,customer.email))
     }
 
 }
